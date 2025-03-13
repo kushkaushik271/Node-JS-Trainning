1fb5abe204df62c770f077d779dc7bd7c24b6893
kushkaushik271 <kush.kaushik@unthinkable.co>
2025-03-13 09:32:37 +0530

    setup node.js project

---
+++ config/default.js
@@ -0,0 +1,12 @@
+module.exports = {
+    PORT: 3000,
+    JWT_SECRET: "Kush@12321",
+    REFRESH_TOKEN_SECRET:"Kaushik@12321",
+    JWT_EXPIRES_IN: "5m",
+    REFRESH_EXPIRES_IN: "24h",
+    DB_URI: "mongodb+srv://kushkaushik:V4hcCag2DrOHV4Ck@cluster0.sfrgm.mongodb.net/",
+    URL: "http://localhost:3000",
+    EMAIL_USER: "kush.kaushik@unthinkable.co",
+    EMAIL_PASS: "sumxytfglfzethvi",
+  };
+  
